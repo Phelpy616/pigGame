3c79f974e8bfc68420fd843fe5315a4172e1f4ad
Phelpy616 <navysealbey@gmail.com>
2024-11-15 23:44:48 -0300

    Update PigGame.js

--- PigGame.js
+++ PigGame.js
@@ -50,7 +50,7 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 200) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add("hidden");
       document
